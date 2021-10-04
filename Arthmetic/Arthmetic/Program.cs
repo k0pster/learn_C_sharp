867e8bf6c0f424ed898d67898ec672efd149d69f
k0pster <39728802+k0pster@users.noreply.github.com>
2021-10-04 19:00:44 +0200

    Add files via upload

---
+++ Arthmetic/Arthmetic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arthmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 5; // x= - ; y=5
+            x = ++y;    // x=6 - ; y=6
+            x = y++;    // x=6 - ; y=7
+            x = --y;    // x=6 - ; y=6
+            x = y--;    // x=6 - ; y=5
+            Console.WriteLine("y = {0}, y++ = {1}",y, y++) ;
+            Console.WriteLine("y po wyświetleniu = {0}",y);
+        }
+    }
+}
