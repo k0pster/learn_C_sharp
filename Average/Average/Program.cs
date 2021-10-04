41e8cef5f12a0a0beb7527550b9f650a7be93e14
k0pster <39728802+k0pster@users.noreply.github.com>
2021-10-04 19:02:52 +0200

    Add files via upload

---
+++ Average/Average/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int computers = 24;
+            int students;
+            double result;
+            Console.WriteLine("Podaj liczbę studentów :");
+            students = Convert.ToInt32((Console.ReadLine()));
+            result = (double)students / computers;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ BMI_calc/BMI_calc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BMI_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight, height, bmi;
+            Console.WriteLine("Kalkulator BMI");
+            Console.WriteLine("Podaj wagę w kg: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost w cm");
+            height = Double.Parse(Console.ReadLine());
+            bmi = weight / (height * height / 10000);
+            Console.WriteLine(bmi);
+        }
+    }
+}
