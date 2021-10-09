fe134b896940254939a5a257cd9c623f3f4c145d
k0pster <39728802+k0pster@users.noreply.github.com>
2021-10-09 17:15:41 +0200

    Add files via upload

---
+++ Reverse_table/Reverse_table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] table1 = new int[] { 10, 11, 12, 13, 14 };
+            int[] reversetable1 = new int[table1.Length];
+            for (int i = table1.Length - 1; i >= 0; i--)
+            {
+                reversetable1[table1.Length - i - 1] = table1[i];
+            }
+            for (int i = 0; i < reversetable1.Length; i++)
+            {
+                Console.WriteLine(reversetable1[i]);
+            }
+        }
+    }
+}
