1b4c1abef3e20942fa2d2402497baae8baae645f
k0pster <39728802+k0pster@users.noreply.github.com>
2021-10-09 16:31:44 +0200

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            Console.WriteLine("Wybierz działanie: +, -, /, *");
+            string operators = Console.ReadLine();
+            Console.WriteLine("Podaj liczbę 1:");
+            double digit1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę 2:");
+            double digit2 = double.Parse(Console.ReadLine());
+            switch (operators)
+            {
+                case "+":
+                    result = digit1 + digit2;
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = digit1 - digit2;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = digit1 / digit2;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = digit1 * digit2;
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine("Wybrano zły znak");
+                    break;
+            }
+        }
+    }
+}
---
+++ power_calculator/power_calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace power_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę do potęgi:");
+            int digit= Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Podaj dodatni wykładnik potęgi:");
+            int power = Convert.ToInt16(Console.ReadLine());
+            if (power > 0)
+            {
+                int power_result = 1;
+                for (int i = 1; i <= power; i++)
+                {
+                    power_result = power_result * digit;
+                    Console.WriteLine("{0} do {1} = {2}", digit, i, power_result);
+                }    
+            }
+        }
+    }
+}
