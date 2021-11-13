6e27c344672a0509582734d9eb8fabb17dbef421
k0pster <39728802+k0pster@users.noreply.github.com>
2021-11-13 15:39:51 +0100

    Add files via upload

---
+++ Character_counter_in_input_text/Character_counter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Character_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfChar = 0;
+            Console.WriteLine("Licznik znaków w tekście");
+            Console.WriteLine("Wpisz tekst");
+            string text = Console.ReadLine();
+            Console.WriteLine("Jaki znak zliczać?");
+            char variable = Convert.ToChar(Console.ReadLine());
+            foreach (char letter in text)
+            {
+                if (letter == variable)
+                {
+                    numberOfChar++;
+                }
+            }
+            Console.WriteLine("Zdanie posiada {1} znaków. Litera '{2}' wystąpiła {0} razy",numberOfChar,text.Length,variable);
+        }
+    }
+}
