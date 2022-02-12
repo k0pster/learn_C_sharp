39ddd7b1cc656a9a49fc3c9dd4ea91c2b67b7746
k0pster <39728802+k0pster@users.noreply.github.com>
2022-02-12 22:19:02 +0100

    Add files via upload

---
+++ AllInOneForDummies/RemovalSpecialChars/RemovalSpecialChars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RemovalSpecialChars
+{
+    class Program
+    {
+        //Tworzymy funkcję pobierająca string oraz tablicę znaków char, które mają być pominięte.
+        //Używamy .Split(), żeby podzielić cały string na pojedyńcze elementy i umieścić je w nowej tablicy stringów
+        //Następnie wypełniamy pusty string łącząc funkcją .Concat() każdy element złączonej tablicy stringów.
+        public static string RemovalSpecialChars(string input, char[] targets)
+        {
+            string[] subStrings = input.Split(targets);
+
+            string output = "";
+
+            foreach (string subString in subStrings)
+            {
+                output = String.Concat(output, subString);
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            //Uruchamiamy funkcję dla pobranego przez użytkownika stringu z usunięciem białych znaków.
+            Console.WriteLine("Podaj string: ");
+            string inputString = Console.ReadLine();
+            char[] whiteSpace = { ' ', '\n','\t' };
+            Console.WriteLine(RemovalSpecialChars(inputString, whiteSpace));
+        }
+    }
+}
